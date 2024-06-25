04abb3dbd4340ec57974782d122f65f4da5d3d78
TheOPedroSantos <pedrosantosmedia@gmail.com>
2024-06-25 15:57:10 +0100

    js file iteration added

--- js.js
+++ js.js
@@ -2,7 +2,25 @@
 
 function getComputerChoice() {
     randomElement = Math.floor(Math.random() * 100);
+    if (randomElement <= 33) {
+        randomElement = "Rock";
+    } else if (randomElement >= 34 && randomElement<= 66) {
+        randomElement = "Paper";
+    } else if (randomElement >= 67) {
+        randomElement = "Scissors";
+    }
+
     return randomElement;
 }
 
+function getHumanChoice() {
+    humanInput = prompt("Insert your move: ");
+    if (!isNaN(humanInput) && humanInput.trim() !== "") {
+        return ("Your move is incorrect. Please insert a move, next time.")
+    } else {
+        return humanInput;
+    }
+}
+
 console.log(getComputerChoice());
+console.log(getHumanChoice());
