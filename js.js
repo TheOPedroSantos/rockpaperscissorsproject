e25545e8b4f0e88ebac5b4b8ae524911df0b3cd4
TheOPedroSantos <pedrosantosmedia@gmail.com>
2024-06-25 17:01:56 +0100

    js file iteration added

--- js.js
+++ js.js
@@ -1,26 +1,52 @@
 // JavaScript code for rock paper scissors project
 
+
+// keep track of the score
+let humanScore = 0;
+let computerScore = 0;
+
+// randomly returns rock, paper or scissors
 function getComputerChoice() {
-    randomElement = Math.floor(Math.random() * 100);
-    if (randomElement <= 33) {
-        randomElement = "Rock";
-    } else if (randomElement >= 34 && randomElement<= 66) {
-        randomElement = "Paper";
-    } else if (randomElement >= 67) {
-        randomElement = "Scissors";
+    computerChoice = Math.floor(Math.random() * 100);
+    if (computerChoice <= 33) {
+        computerChoice = "Rock".toUpperCase();
+    } else if (computerChoice >= 34 && computerChoice<= 66) {
+        computerChoice = "Paper".toUpperCase() ;
+    } else if (computerChoice >= 67) {
+        computerChoice = "Scissors".toUpperCase() ;
     }
-
-    return randomElement;
+    return computerChoice;
 }
 
+// takes the user choice and returns it
 function getHumanChoice() {
-    humanInput = prompt("Insert your move: ");
-    if (!isNaN(humanInput) && humanInput.trim() !== "") {
+    humanChoice = prompt("Insert your move: ");
+    if (!isNaN(humanChoice) && humanChoice.trim() !== "") {
         return ("Your move is incorrect. Please insert a move, next time.")
+        // resetGame(); this function is missing for now
     } else {
-        return humanInput;
+        return humanChoice;
     }
 }
+
+// logic to play a single round
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toUpperCase(); // make the humanChoice uppercase for case sensibility
+
+    console.log();
+    humanScore ++;
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+playRound(humanSelection, computerSelection);
+
+
+function playGame() {
+
+}
+
 
 console.log(getComputerChoice());
 console.log(getHumanChoice());
+console.log(humanScore, computerScore);
