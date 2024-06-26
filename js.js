82dc5a521ea403b98cb8844fb1f77fcf64585c68
TheOPedroSantos <pedrosantosmedia@gmail.com>
2024-06-26 15:10:08 +0100

    js file iteration added

--- js.js
+++ js.js
@@ -7,10 +7,11 @@ let computerScore = 0;
 
 // randomly returns rock, paper or scissors
 function getComputerChoice() {
-    const randomValue = Math.floor(Math.random() * 3);
-    if (randomValue === 0) {
+    const computerChoice = Math.floor(Math.random() * 3);
+
+    if (computerChoice === 0) {
         return "rock";
-    } else if (randomValue === 1) {
+    } else if (computerChoice === 1) {
         return "paper";
     } else {
         return "scissors";
@@ -20,27 +21,27 @@ function getComputerChoice() {
 // takes the user choice and returns it
 function getHumanChoice() {
     let humanChoice = prompt("Insert your move").toLowerCase(); // convert to lowercase
+
     if (humanChoice !== "rock" && humanChoice !== "paper" && humanChoice !== "scissors") {
         console.log("Your move is incorrect. Please insert a move, next time.");
         return null; // Indicates an invalid choice
-        // resetGame(); this function is missing for now
     } else {
         return humanChoice;
     }
 }
 
 // logic to play a single round
-function playRound(humanChoice, computerChoice) {
+function playRound(humanChoice, computerChoice) {    
     if (!humanChoice) { // if the value humanChoice is falsy, then:
         console.log("No valid move.");
         return;
     }
 
     humanChoice = humanChoice.toLowerCase(); // make the humanChoice lowercase for case sensibility
-    console.log('Human choice: ${humanChoice}, Computer choice: ${computerChoice}'); // returns the choices
+    console.log(`Human choice: ${humanChoice}, Computer choice: ${computerChoice}`); // returns the choices
 
-    if (humanChoice === humanChoice) {        
-            console.log("It's a tie! Play again.");
+    if (humanChoice === computerChoice) {        
+            console.log("It's a tie!");
     } else if (
         humanChoice === "rock" &&  computerChoice === "scissors" ||
         humanChoice === "paper" && computerChoice === "rock" ||
@@ -54,17 +55,22 @@ function playRound(humanChoice, computerChoice) {
     }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
-
-
+// logic to play the whole game
 function playGame() {
-   
+   for (let round = 0; round < 5; round++) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+   }
+
+   console.log(`FINAL SCORE: Human - ${humanScore}, Computer - ${computerScore}`);
+   if (humanScore > computerScore) {
+    console.log("Congrats, you win!");
+   } else if (humanScore === computerScore) {
+    console.log("It's a tie! Play again.");
+   } else {
+    console.log("Too bad. Computer win!");
+   }
 }
 
-
-console.log(getComputerChoice());
-console.log(getHumanChoice());
-console.log(humanScore, computerScore);
+playGame();
